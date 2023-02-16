42d1947e5d94ea27273680e8f87aaa28b2ccaf23
igorBekin <igor_bekin@mail.ru>
2023-02-16 11:28:55 +0300

    Initial Commit

---
+++ Sun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sun
+//
+//  Created by Игорь Бекин on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
