e005c8a66388cc63ab4e8f92b29eedf386aaef70
igorBekin <igor_bekin@mail.ru>
2023-02-21 13:23:26 +0300

    Пробовал сам менять

--- Sun/ViewController.swift
+++ Sun/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var isSun: Bool = false
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var skyImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
+        skyImageView.tintColor = .brown
+        changeButton.backgroundColor = .blue
+        
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        print("Нажатие")
+        if isSun { // 1
+            skyImageView.image = UIImage(systemName: "moon") // 2
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max") // 3
+        }
+        isSun.toggle() // 4
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        print("Нажатие")
+        
+    }
 }
 
